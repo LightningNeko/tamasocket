0d968d1e80707198f23fea1f1285524fee144ae0
LightningNeko <80431773+LightningNeko@users.noreply.github.com>
2022-09-21 17:44:02 -0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+
+
+// Base de datos donde guardar la sesión del bot, puede ser "mongodb" o "sqlite".
+exports.dbmode = "sqlite";
+
+
+// URL de MongoDB, necesaria si se utiliza "mongodb" como base de datos.
+exports.mongo = "";
+
+
+// Token del bot.
+exports.cookie = "";
